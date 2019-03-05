83ccb12ca5d8b856ef9c4ab70e8736c9cb75ecb2
oscargarciahernandez <garcia96.oscar@gmail.com>
2019-03-05 23:21:40 +0100

    cosmobox empezando a ver como se puede hacer el requests

---
+++ cosmobox_download.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  5 18:16:58 2019
+
+@author: Oscar
+"""
+
+GET /d/6ieqzy2s72dz4q3jasjo4kzjlx44z6drov3qnmywp4pdl76buuklp5r3gdpko4t5axwgelll/Avgustin%20-%20Eject%20(Dushe%20Label).zip HTTP/1.1
+Host: cosmobox.org:182
+Connection: keep-alive
+Cache-Control: max-age=0
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36 OPR/58.0.3135.79
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
+Accept-Encoding: gzip, deflate
+Accept-Language: es-ES,es;q=0.9
+Cookie: login=oscargarciahernandez; xfss=pcphpg4qxy35yfqd; file_code=vqf0trm2p6dq; aff=5; ref_url=https%3A%2F%2Fwww.electrobuzz.net%2F2019%2F03%2Favgustin-eject-azteca-remix-dushel131%2F; dt=1551805711
+
+
+
+GET /d/6ieq7y2s72dz4q3jasjoskdwac72igpzjxisvekoxw6kybx4l7r4wqa2w4grfne7gyf7utfk/Osunlade,%20Carlos%20Mena,%20Azania%20Brothers%20-%20Desire%20(Ocha%20Mzansi).zip HTTP/1.1
+Host: cosmobox.org:182
+Connection: keep-alive
+Cache-Control: max-age=0
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36 OPR/58.0.3135.79
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
+Accept-Encoding: gzip, deflate
+Accept-Language: es-ES,es;q=0.9
+Cookie: login=oscargarciahernandez; xfss=pcphpg4qxy35yfqd; aff=5; file_code=qr9m9mmpxs42; ref_url=https%3A%2F%2Fwww.electrobuzz.net%2F2019%2F03%2Fosunlade-carlos-mena-azania-brothers-desire-och053%2F; dt=1551807389
